aed266696dcb0851a2b943243fe020a379bc5466
Gyanrp12 <gpa.globaliasoft@gmail.com>
2022-07-18 16:24:06 +0530

    userview updated

--- myapp/views.py
+++ myapp/views.py
@@ -12,6 +12,8 @@ from .models import User
 import jwt,datetime
 from django.contrib.auth import authenticate
 from rest_framework.permissions import IsAuthenticated 
+from rest_framework_jwt.utils import jwt_decode_handler
+
 
 # Create your views here.
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -19,8 +21,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
     return {
-        'refresh': str(refresh),
-        'access':str(refresh.access_token),
+        # 'refresh': str(refresh),
+        str(refresh.access_token),
     }
  
 class RegisterView(APIView):
@@ -33,13 +35,14 @@ class RegisterView(APIView):
 
 class LoginView(APIView):
     def post(self,request):
-        email = request.data['email']
-        password = request.data['password']
-
+        # ser = UserSerializer(data=request.data)
+        # ser.is_valid(raise_exception=True)
+        # email = request.data['email']
+        # password = request.data['password']
         try:
-                usr = User.objects.get(email=email)
+                usr = User.objects.get(email= request.data['email'])
                 try:
-                    user = authenticate(email=email,password=password)
+                    user = authenticate(email=request.data['email'],password=request.data['password'])
                     if user is not None :
                         token = get_tokens_for_user(usr)
                      
@@ -47,31 +50,45 @@ class LoginView(APIView):
                         response.set_cookie(key='jwt', value=token)
                         response.data = {
                             'jwt': token
+                       
                     
-                        }
+                        } 
                  
                         return response
+                    else:
+                        return Response({"msg":"password is incorrect"})
                 except:
                     return Response({'msg':"errors"})
         except:
                 return Response({'msg':"email is invalid"})
 
 class UserView(APIView):
-    permissions_classes = [IsAuthenticated]
+    # permission_classes  = [IsAuthenticated]
+   
     def get(self, request):
-         ser = UserSerializer(request.user)
-        
-         return Response(ser.data)
-    
-    
+        try:
+            if request.COOKIES['jwt']  is not None:
+                ser = UserSerializer(request.user)
+                return Response(ser.data)
+               
+            else:
+                return Response(ser.errors)
+        except:
+            return Response({"msg":"token is invalid"})
+        # token = request.COOKIES['jwt']
+        # print('payload'+ str(settings.SECRET_KEY))
+        # try:
+        #     token = request.COOKIES.get('jwt')
+        #     print(token)
+        #     payload = jwt.decode(token)
+        #     user = User.objects.get(id=payload['user_id'])
+        # except:
+        #     return Response({"msg":"token is invalid"})
 class LogoutView(APIView):
     def post(self, request):
-     
         response = Response()
         response.delete_cookie('jwt')
         response.data={
             'msg':'logout'
-            
-        }
-
+         }
         return response
--- mysite/urls.py
+++ mysite/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('login/',LoginView.as_view()),
     path('user/',UserView.as_view()),
     path('logout/',LogoutView.as_view()),
+    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
